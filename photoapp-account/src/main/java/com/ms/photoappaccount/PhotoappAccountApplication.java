d8c4f87bfcd78f1c0ea5416ebe4c8a549c4e1123
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-05-27 12:42:16 +0100

    chore: setup boilerplate for multimodule gradle project

---
+++ photoapp-account/src/main/java/com/ms/photoappaccount/PhotoappAccountApplication.java
@@ -0,0 +1,13 @@
+package com.ms.photoappaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappAccountApplication.class, args);
+	}
+
+}
---
+++ photoapp-account/src/test/java/com/ms/photoappaccount/PhotoappAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.photoappaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
